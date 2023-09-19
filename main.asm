b0ded20e949ec7826fecac12dbb0c45d8aa723e3
adriannasim <adriannasim@gmail.com>
2023-09-20 03:08:29 +0800

    HAVENT DONE CHECKING PSW AND USERNAME

--- main.asm
+++ main.asm
@@ -1,38 +1,156 @@
-TITLE   ASSIGNMENT  MAIN FILE
+TITLE   ASSIGNMENT  TESTMENU
 
 .MODEL  SMALL
 .STACK  64
 .DATA
 
-;PROMPTS
+;START OF VARIABLES DECLARATION
+;-------------------------------------------------------------------------------------
+;=================================READ FILE VARIABLES=================================
+;MAINMENU
+MAINMENU    DB      "menu.txt", 0           ;FILENAME
+MMBUFFER    DB      862 DUP(?)              ;BUFFER TO STORE FILE CONTENT
+FILE_HANDLE DW      ?                       ;FILE HANDLE
+BYTESREAD   DW      0
 
+;=================================NEW LINE VARIABLES==================================
+CR          EQU     0DH                     ;CARRIAGE RETURN SHORT FORM
+LF          EQU     0AH                     ;LINE FEED SHORT FORM
 
-;VARIABLES
+;=================================PRINTING VARIABLES==================================
+ERRFILE     DB      "FILE ERROR$"
+GOODLOGIN   DB      "LOGIN SUCCESSFUL", LF, CR, "$"
+BADLOGIN    DB      "WRONG USERNAME/PASSWORD. PLEASE TRY AGAIN.", LF, CR, "$"
+MSGUSER     DB      "USERNAME > $"
+MSGPSW      DB      "PASSWORD > $"
 
+;===================================STRING INPUT======================================
+;USERNAME
+INUSER      LABEL   BYTE
+MAXUSER     DB      20
+ACTUALUSER  DB      ?
+SPACEUSER   DB      20 DUP(' ')
 
-;OTHER FUNCTIONS
-CR      EQU     0DH                 ;CARRIAGE RETURN SHORT FORM
-LF      EQU     0AH                 ;LINE FEED SHORT FORM
+;PASSWORD
+INPSW       LABEL   BYTE
+MAXPSW      DB      20
+ACTUALPSW   DB      ?
+SPACEPSW    DB      20 DUP(' ')
 
-;-------------------------------------------------------------------------
-;START OF HEADER
-.CODE                               ;DEFINE CODE SEGMENT
+;================================USERNAME AND PASSWORD=================================
+CHECKUSER   DB      "ADMIN$"
+CHECKPSW    DB      "HUNGRY$"
 
-    MAIN    PROC FAR                ;MAIN PROCEDURE START
+;END OF VARIABLES DECLARATION
+;-------------------------------------------------------------------------------------
+;==================================START OF HEADER====================================
+.CODE                                       ;DEFINE CODE SEGMENT
+
+    MAIN    PROC FAR                        ;MAIN PROCEDURE START
 
     MOV     AX, @DATA
-    MOV     DS, AX                  ;SET ADDRESS OF DATA SEGMENT IN DS
-;END OF HEADER
+    MOV     DS, AX                          ;SET ADDRESS OF DATA SEGMENT IN DS
+;====================================END OF HEADER====================================
 
-;START OF MAIN MENU
+;=================================START OF MAIN MENU==================================
+    ;OPEN THE FILE
+    ; MOV     AH, 3DH                             ;DOS FUNCTION TO OPEN A FILE
+    ; MOV     AL, 0                               ;READ-ONLY MODE
+    ; LEA     DX, MAINMENU                        ;LOAD THE FILENAME INTO DX
+    ; INT     21H
 
-;END OF MAIN MENU
+    ; JC      FILE_ERROR                          ;JUMP IF ERROR
 
-;START OF FOOTER
-    MOV     AX, 4C00H
+    ; ;READ THE FILE CONTENT
+    ; MOV     FILE_HANDLE, AX                     ;STORE THE FILE HANDLE
+    ; MOV     AH, 3FH                             ;DOS FUNCTION TO READ FROM A FILE
+    ; MOV     BX, FILE_HANDLE                     ;FILE HANDLE
+    ; MOV     CX, 862                             ;NUMBER OF BYTES TO READ AT A TIME
+    ; LEA     DX, MMBUFFER                        ;BUFFER TO STORE THE CONTENT
+    ; INT     21H
+
+    ; JC      FILE_ERROR                          ;JUMP IF ERROR
+
+    ; ;DISPLAY THE FILE CONTENT
+    ; MOV     AH, 09H                             ;DOS FUNCTION TO DISPLAY A STRING
+    ; LEA     DX, MMBUFFER                        ;LOAD THE BUFFER ADDRESS
+    ; INT     21H
+
+    ; ;CLOSE THE FILE
+    ; MOV     AH, 3EH                             
+    ; MOV     BX, FILE_HANDLE                     
+    ; INT     21H                                 ;DOS FUNCTION TO CLOSE A FILE
+    ; JMP     EXIT
+
+    ; ;ERROR HANDLER
+    ; FILE_ERROR:     MOV     AH, 09H
+    ;                 LEA     DX, ERRFILE
+    ;                 INT     21H
+    ;                 JMP     EXIT
+
+    ;ASK FOR USERNAME
+    LOGIN:  MOV     AH, 0AH                             ;DOS FUNCTION TO DISPLAY STRING
+            LEA     DX, MSGUSER
+            INT     21H
+
+            MOV     AH, 0AH                             ;DOS FUNCTION TO ACCEPT STRING
+            LEA     DX, INUSER
+            INT     21H
+
+            .386                                        ;CALL FOR ADVANCE FUNCTION
+            MOVZX   BX, ACTUALUSER
+            MOV     SPACEUSER[BX], '$'                  ;TERMINATE USER INPUT STRING WITH $
+
+            MOV     AH, 02H
+            MOV     DL, CR
+            INT     21H
+            MOV     DL, LF
+            INT     21H                                 ;NEXT LINE
+
+            ;ASK FOR PASSWORD
+            MOV     AH, 0AH                             ;DOS FUNCTION TO DISPLAY STRING
+            LEA     DX, MSGPSW
+            INT     21H
+
+            MOV     AH, 0AH                             ;DOS FUNCTION TO ACCEPT STRING
+            LEA     DX, INPSW
+            INT     21H
+
+            .386                                        ;CALL FOR ADVANCE FUNCTION
+            MOVZX   BX, ACTUALPSW
+            MOV     SPACEPSW[BX], '$'                   ;TERMINATE USER INPUT STRING WITH $
+
+            MOV     AH, 02H
+            MOV     DL, CR
+            INT     21H
+            MOV     DL, LF
+            INT     21H                                 ;NEXT LINE
+
+    ;CHECK USERNAME AND PASSWORD
+    CMP     INUSER, CHECKUSER
+    CMP     INPSW, CHECKPSW
+    JE      MAINPAGE
+
+    ;WRONG USERNAME AND PASSWORD
+    MOV     AH, 0AH                             ;DOS FUNCTION TO DISPLAY STRING
+    LEA     DX, BADLOGIN
     INT     21H
+    JMP     LOGIN
 
-    MAIN    ENDP
     
+;===================================END OF MAIN MENU===================================
+
+;=================================START OF MAIN PAGE===================================
+    MAINPAGE:   MOV     AH, 0AH                       ;DOS FUNCTION TO DISPLAY STRING
+                LEA     DX, GOODLOGIN
+                INT     21H
+;==================================END OF MAIN PAGE====================================
+
+;===================================START OF FOOTER====================================
+    EXIT:   MOV     AX, 4C00H
+            INT     21H
+
+            MAIN    ENDP
+
 END MAIN
-;END OF FOOTER
+;====================================END OF FOOTER=====================================
--- menu.asm
+++ menu.asm
@@ -1,64 +1,78 @@
-TITLE   ASSIGNMENT  TEST MENU
+TITLE   ASSIGNMENT  TESTMENU
 
 .MODEL  SMALL
 .STACK  64
 .DATA
 
 ;START OF VARIABLES DECLARATION
-;READ FILE VARIABLES
+;-------------------------------------------------------------------------------------
+;=================================READ FILE VARIABLES=================================
 ;MAINMENU
-MAINMENU    DB      "TEST.txt", 0   ;FILENAME
-MMBUFFER    DB      1000 DUP(?)          ;BUFFER TO STORE FILE CONTENT
-FILE_HANDLE DW      ?                    ;FILE HANDLE
+MAINMENU    DB      "menu.txt", 0           ;FILENAME
+MMBUFFER    DB      862 DUP(?)              ;BUFFER TO STORE FILE CONTENT
+FILE_HANDLE DW      ?                       ;FILE HANDLE
+BYTESREAD   DW      0
 
-;NEW LINE VARIABLES
-CR          EQU     0DH                 ;CARRIAGE RETURN SHORT FORM
-LF          EQU     0AH                 ;LINE FEED SHORT FORM
+;=================================NEW LINE VARIABLES==================================
+CR          EQU     0DH                     ;CARRIAGE RETURN SHORT FORM
+LF          EQU     0AH                     ;LINE FEED SHORT FORM
+
+;=================================PRINTING VARIABLES==================================
+ERRMSG      DB      "FILE ERROR$"
+
 ;END OF VARIABLES DECLARATION
-;-------------------------------------------------------------------------
-;START OF HEADER
-.CODE                                   ;DEFINE CODE SEGMENT
+;-------------------------------------------------------------------------------------
+;==================================START OF HEADER====================================
+.CODE                                       ;DEFINE CODE SEGMENT
 
-    MAIN    PROC FAR                    ;MAIN PROCEDURE START
+    MAIN    PROC FAR                        ;MAIN PROCEDURE START
 
     MOV     AX, @DATA
-    MOV     DS, AX                      ;SET ADDRESS OF DATA SEGMENT IN DS
-;END OF HEADER
+    MOV     DS, AX                          ;SET ADDRESS OF DATA SEGMENT IN DS
+;====================================END OF HEADER====================================
 
-;START OF MAIN MENU
-    ;START OF MAIN MENU
-    ;OPEN FILE
-    MOV     AH ,3DH				        ;OPEN FILE COMMAND
-    LEA     DX, MAINMENU
-    MOV     AL, 0
-    INT     21H
-    MOV     FILE_HANDLE, AX
-
-    ;READ FILE
-    MOV     AH, 3FH
-    MOV     BX, FILE_HANDLE
-    LEA     DX, MMBUFFER       
-    MOV     CX, 1000		            ;NUMBER OF BYTES TO READ FROM THE FILE
+;=================================START OF MAIN MENU==================================
+    ;OPEN THE FILE
+    MOV     AH, 3DH                             ;DOS FUNCTION TO OPEN A FILE
+    MOV     AL, 0                               ;READ-ONLY MODE
+    LEA     DX, MAINMENU                        ;LOAD THE FILENAME INTO DX
     INT     21H
 
-    ;DISPLAY FILE
-    MOV     AH, 09H
-    LEA     DX, MMBUFFER
+    JC      FILE_ERROR                          ;JUMP IF ERROR
+
+    ;READ THE FILE CONTENT
+    MOV     FILE_HANDLE, AX                     ;STORE THE FILE HANDLE
+    MOV     AH, 3FH                             ;DOS FUNCTION TO READ FROM A FILE
+    MOV     BX, FILE_HANDLE                     ;FILE HANDLE
+    MOV     CX, 862                             ;NUMBER OF BYTES TO READ AT A TIME
+    LEA     DX, MMBUFFER                        ;BUFFER TO STORE THE CONTENT
     INT     21H
 
-    ;CLOSE FILE
-    MOV     AH, 3EH
-    MOV     BX, FILE_HANDLE	            ;FILE HANDLE 
-    INT     21H
-    ;END OF MAIN MENU
+    JC      FILE_ERROR                          ;JUMP IF ERROR
 
-;END OF MAIN MENU
-
-;START OF FOOTER
-    MOV     AX, 4C00H
+    ;DISPLAY THE FILE CONTENT
+    MOV     AH, 09H                             ;DOS FUNCTION TO DISPLAY A STRING
+    LEA     DX, MMBUFFER                        ;LOAD THE BUFFER ADDRESS
     INT     21H
 
-    MAIN    ENDP
+    ;CLOSE THE FILE
+    MOV     AH, 3EH                             
+    MOV     BX, FILE_HANDLE                     
+    INT     21H                                 ;DOS FUNCTION TO CLOSE A FILE
+    JMP     EXIT
+
+    ;ERROR HANDLER
+    FILE_ERROR:     MOV     AH, 09H
+                    LEA     DX, ERRMSG
+                    INT     21H
+                    JMP     EXIT
+;===================================END OF MAIN MENU===================================
+
+;===================================START OF FOOTER====================================
+    EXIT:   MOV     AX, 4C00H
+            INT     21H
+
+            MAIN    ENDP
 
 END MAIN
-;END OF FOOTER
+;====================================END OF FOOTER=====================================
